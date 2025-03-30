e232e4d7940b0cfe2202b127bc33f85b2dd50398
pppB02 <pappbalazs02@gmail.com>
2025-03-30 20:04:59 +0200

    fixed some  mobil and tabel ui bugs

--- ham-menu.js
+++ ham-menu.js
@@ -1,8 +1,12 @@
 const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
+const html = document.getElementById("html");
+const body = document.getElementById("body");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
+    html.classList.toggle("active");
+    body.classList.toggle("active");
 })
